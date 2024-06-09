0b2b177183fbed8b87a65438601adf9d3e4dc920
王子恒 <wzhzzmzzy@gmail.com>
2024-06-09 14:12:29 +0000

    update

---
+++ src/validators/pattern.ts
@@ -0,0 +1,21 @@
+import { ExecuteValidator } from "../types/async-validator";
+import { rules } from "../rules";
+import { isEmptyValue } from "../utils";
+
+const pattern: ExecuteValidator = (rule, value, callback, source, options) => {
+  const errors: string[] = [];
+  const validate =
+    rule.required || (!rule.required && source.hasOwnProperty(rule.field));
+  if (validate) {
+    if (isEmptyValue(value, "string") && !rule.required) {
+      return callback();
+    }
+    rules.required(rule, value, source, errors, options);
+    if (!isEmptyValue(value, "string")) {
+      rules.pattern(rule, value, source, errors, options);
+    }
+  }
+  callback(errors);
+};
+
+export default pattern;
