dc4fe4f1649ce8c755a57402d9677c4f49cbb188
王子恒 <wzhzzmzzy@gmail.com>
2024-06-10 06:52:29 +0000

    Add validation in playground apps

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
+import Schema from '../../src'
+
+const validate = (rule, source) => {
+  return new Schema(rule).validate(source);
+}
 
 const App = () => {
-  return <Button>App</Button>;
+  const rule = {};
+  const source = {};
+  const [errors, setErrors] = useState([])
+  const runValidate = async () => {
+    try {
+      await validate(rule, source);
+      setErrors([])
+    } catch (e) {
+      setErrors(e.errors)
+    }
+    
+  }
+  
+  return (
+    <>
+    <div>{JSON.stringify(errors)}</div>
+    <Button onClick={runValidate}>App</Button>
+    </>
+  );
 };
 
 export default App;
