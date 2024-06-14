a963ab6c695cee7f9761b4cb0dfe709049e3de32
王子恒 <wzhzzmzzy@gmail.com>
2024-06-14 06:32:49 +0000

    fix: options arg optional

--- src/rules/url.ts
+++ src/rules/url.ts
@@ -6,16 +6,16 @@ export default () => {
     return urlReg;
   }
 
-  const word = '[a-fA-F\\d:]';
-  const b = options =>
+  const word = "[a-fA-F\\d:]";
+  const b = (options) =>
     options && options.includeBoundaries
       ? `(?:(?<=\\s|^)(?=${word})|(?<=${word})(?=\\s|$))`
-      : '';
+      : "";
 
   const v4 =
-    '(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)(?:\\.(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)){3}';
+    "(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)(?:\\.(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)){3}";
 
-  const v6seg = '[a-fA-F\\d]{1,4}';
+  const v6seg = "[a-fA-F\\d]{1,4}";
   const v6 = `
 (?:
 (?:${v6seg}:){7}(?:${v6seg}|:)|                                    // 1:2:3:4:5:6:7::  1:2:3:4:5:6:7:8
@@ -28,8 +28,8 @@ export default () => {
 (?::(?:(?::${v6seg}){0,5}:${v4}|(?::${v6seg}){1,7}|:))             // ::2:3:4:5:6:7:8  ::2:3:4:5:6:7:8  ::8             ::1.2.3.4
 )(?:%[0-9a-zA-Z]{1,})?                                             // %eth0            %1
 `
-    .replace(/\s*\/\/.*$/gm, '')
-    .replace(/\n/g, '')
+    .replace(/\s*\/\/.*$/gm, "")
+    .replace(/\n/g, "")
     .trim();
 
   // Pre-compile only the exact regexes because adding a global flag make regexes stateful
@@ -37,36 +37,36 @@ export default () => {
   const v4exact = new RegExp(`^${v4}$`);
   const v6exact = new RegExp(`^${v6}$`);
 
-  const ip = options =>
+  const ip = (options) =>
     options && options.exact
       ? v46Exact
       : new RegExp(
           `(?:${b(options)}${v4}${b(options)})|(?:${b(options)}${v6}${b(
             options,
           )})`,
-          'g',
+          "g",
         );
 
-  ip.v4 = (options?) =>
+  ip.v4 = (options?: unknown) =>
     options && options.exact
       ? v4exact
-      : new RegExp(`${b(options)}${v4}${b(options)}`, 'g');
-  ip.v6 = (options?) =>
+      : new RegExp(`${b(options)}${v4}${b(options)}`, "g");
+  ip.v6 = (options?: unknown) =>
     options && options.exact
       ? v6exact
-      : new RegExp(`${b(options)}${v6}${b(options)}`, 'g');
+      : new RegExp(`${b(options)}${v6}${b(options)}`, "g");
 
   const protocol = `(?:(?:[a-z]+:)?//)`;
-  const auth = '(?:\\S+(?::\\S*)?@)?';
+  const auth = "(?:\\S+(?::\\S*)?@)?";
   const ipv4 = ip.v4().source;
   const ipv6 = ip.v6().source;
-  const host = '(?:(?:[a-z\\u00a1-\\uffff0-9][-_]*)*[a-z\\u00a1-\\uffff0-9]+)';
+  const host = "(?:(?:[a-z\\u00a1-\\uffff0-9][-_]*)*[a-z\\u00a1-\\uffff0-9]+)";
   const domain =
-    '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
+    "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*";
   const tld = `(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))`;
-  const port = '(?::\\d{2,5})?';
+  const port = "(?::\\d{2,5})?";
   const path = '(?:[/?#][^\\s"]*)?';
   const regex = `(?:${protocol}|www\\.)${auth}(?:localhost|${ipv4}|${ipv6}|${host}${domain}${tld})${port}${path}`;
-  urlReg = new RegExp(`(?:^${regex}$)`, 'i');
+  urlReg = new RegExp(`(?:^${regex}$)`, "i");
   return urlReg;
 };
